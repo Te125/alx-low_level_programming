0068fec79c43ff5136e8a7c59e04b499b4027d1b
Te125 <joyibom26@gmail.com>
2023-03-17 23:12:25 +0000

    Helo c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - Entry
+ * main - Entry point
  * Return: 1
  */
+int main(void)
 {
-	write(2, "and that piece of art i useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art i useful\" - Dora Korpar, 2015-10-19\n""
 	return (1);
 }
