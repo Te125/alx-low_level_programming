74ea69d34c4a57a28c9187716ebc94ca3b40a517
Te125 <joyibom26@gmail.com>
2023-04-01 19:59:40 +0000

    Nested loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -24,12 +24,13 @@ int main(void)
 			printf(" FizzBuzz");
 		} else if (i == 1)
 		{
-			printf("%d ", i);
+			printf("%d", i);
 		} else
 		{
 			printf(" %d", i);
 		}
 	}
 	printf("\n");
+
 	return (0);
 }
