22e87268d0e2599084a8c4292903b53af3ba941b
Te125 <joyibom26@gmail.com>
2023-08-03 23:06:18 +0100

    bit

--- 0x15-file_io/1-main.c
+++ 0x15-file_io/1-main.c
@@ -3,20 +3,20 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * main - check the code      
  *
  * Return: Always 0.
  */
 int main(int ac, char **av)
 {
-    int res;
+  int res;
 
-    if (ac != 3)
+  if (ac != 3)
     {
-        dprintf(2, "Usage: %s filename text\n", av[0]);
-        exit(1);
+      dprintf(2, "Usage: %s filename text\n", av[0]);
+      exit(1);
     }
-    res = create_file(av[1], av[2]);
-    printf("-> %i)\n", res);
-    return (0);
+  res = create_file(av[1], av[2]);
+  printf("-> %i)\n", res);
+  return (0);
 }
