41c52ecbe249b4c544585fc8923169d4ce9315c7
Te125 <joyibom26@gmail.com>
2023-08-08 13:21:52 +0100

    file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,11 +8,18 @@
 #include <string.h>
 #include <sys/types.h>
 #include <fcntl.h>
+#include <elf.h>
 
 int _putchar(char c);
 size_t _strlen(char *str);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int main(int __attribute__((__unused__)) argc, char *argv[]);
+void check97(int argc);
+void check98(ssize_t check, char *file, int fd_from, int fd_to);
+void check99(ssize_t check, char *file, int fd_from, int fd_to);
+void check100(int check, int fd);
+int main(int argc, char *argv[]);
 
 #endif
