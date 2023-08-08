5197f32a33d7cc8876c0eaed8faab3cb2b4bcc21
Te125 <joyibom26@gmail.com>
2023-08-08 12:31:30 +0100

    file i/o

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,29 +9,37 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, len, at;
+	int fp;
+	ssize_t d;
+	size_t len;
+	mode_t p = S_IRUSR | S_IWUSR;
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
-
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-	if (fd == -1)
+	fp = open(filename, O_WRONLY | O_CREAT | O_TRUNC, p);
+	if (fp == -1)
 	{
+		perror("Error creating the file");
 		return (-1);
 	}
-
 	if (text_content != NULL)
 	{
-		for (at = 0; text_content[at]; at++)
-			len = write(fd, text_content, at);
-		if (len == -1)
+		len = strlen(text_content);
+		d = write(fp, text_content, len);
+
+		if (d == -1 || (size_t)d != len)
 		{
+			perror("Error writing to the file");
+			close(fp);
 			return (-1);
 		}
 	}
-
-	close(fd);
+	if (close(fp) == -1)
+	{
+		perror("Error closing the file");
+		return (-1);
+	}
 	return (1);
 }
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,40 +9,29 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd;
-	ssize_t len;
+	FILE *fp;
+	size_t d, len;
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
-	fd = open(filename, O_WRONLY | O_APPEND);
-	if (fd == -1)
+	fp = fopen(filename, "a");
+	if (fp == NULL)
 	{
 		return (-1);
 	}
 	if (text_content != NULL)
 	{
-		len = write(fd, text_content, _strlen(text_content));
+		len = strlen(text_content);
+		d = fwrite(text_content, sizeof(char), len, fp);
+
+		if (d != len)
+		{
+			fclose(fp);
+			return (-1);
+		}
 	}
-	close(fd);
-	if (len == -1)
-	{
-		return (-1);
-	}
+	fclose(fp);
 	return (1);
-}
-
-/**
- * _strlen - finds the length of a string
- * @str: pointer to the string
- *
- * Return: length of the string
- */
-size_t _strlen(char *str)
-{
-	size_t i;
-
-	for (i = 0; str[i]; i++)
-		return (i);
 }
